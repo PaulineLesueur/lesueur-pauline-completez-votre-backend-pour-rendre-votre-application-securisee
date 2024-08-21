35679b0cae033d352b8b591d42e363090f7aa3f3
Scarounine <paulinelesueur93@gmail.com>
2024-08-21 16:36:52 +0200

    Add login.html file

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.nnk.springboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*@Configuration
+public class SpringSecurityConfig {
+
+   @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/css/*", "/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
+*/
